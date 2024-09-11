8a278a406fb5819282a303a6b531f83980e7cd82
oneonion <1434760180@qq.com>
2024-09-11 15:32:51 +0800

    add char-max_length

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -19,7 +19,7 @@ class Counters(models.Model):
 
 class RequestHistory(models.Model):
     id = models.AutoField
-    requestUser = models.CharField(default='noName')
+    requestUser = models.CharField(default='noName', max_length=128)
     location = models.TextField(default='北京市海淀区西土城路')
     createdAt = models.DateTimeField(default=datetime.now(),)
 
